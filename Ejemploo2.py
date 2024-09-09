87338a1cd1013fb0eda1c2e63013435b79fa4b68
labf402 <74166137@continental.edu.pe>
2024-09-09 15:15:12 -0500

    Modificacion del ejercicio 02

--- Ejemploo2.py
+++ Ejemploo2.py
@@ -1,12 +1,12 @@
-def calcular(a, b, c):
-    res = a * b + c
-    return res
+def calcular(variable1, variable2, variable3):
+    resultado = variable1 * variable2 + variable3
+    return resultado
 
 def principal():
-    x = 5
-    y = 3
-    z = 7
-    resultado = calcular(x, y, z)
+    numero1 = float(input("Ingresar numero 1: "))
+    numero2 = float(input("Ingresar numero 2: "))
+    numero3 = float(input("Ingresar numero 3: "))
+    resultado = calcular(numero1, numero2, numero3)
     print("El resultado es:", resultado)
 
 principal()
